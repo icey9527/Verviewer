3ec09e0b31632237f4e6bf5b074feb81f34d28ab
icey9527 <33127620@qq.com>
2026-04-12 19:28:28 +0800

    lfb

--- Images/LEAF LFB.cs
+++ Images/LEAF LFB.cs
@@ -1,4 +1,4 @@
-using System.Drawing;
+﻿using System.Drawing;
 using System.IO;
 using Verviewer.Core;
 using Utils;
@@ -38,6 +38,9 @@ namespace Verviewer.Images
                     return null;
 
                 if (TryDecodeIndexedAlphaBitmap(bmpBytes, out Bitmap? bmp))
+                    return bmp;
+
+                if (TryDecodeLeaf32Bitmap(bmpBytes, out bmp))
                     return bmp;
 
                 return Image.FromStream(new MemoryStream(bmpBytes, writable: false));
@@ -106,6 +109,77 @@ namespace Verviewer.Images
                         row[dst + 1] = palette[pal + 1];
                         row[dst + 2] = palette[pal + 2];
                         row[dst + 3] = (byte)alpha;
+                    }
+
+                    ImageUtils.CopyRowToBitmap(bmpData, y, row, stride);
+                }
+
+                return true;
+            }
+            catch
+            {
+                createdBmp.Dispose();
+                bmp = null;
+                return false;
+            }
+            finally
+            {
+                ImageUtils.UnlockBitmap(bmpData, createdBmp);
+            }
+        }
+
+        static bool TryDecodeLeaf32Bitmap(byte[] bmpBytes, out Bitmap? bmp)
+        {
+            bmp = null;
+
+            if (bmpBytes.Length < BitmapFileHeaderSize + BitmapInfoHeaderSize)
+                return false;
+
+            int pixelOffset = ReadInt32LE(bmpBytes, 10);
+            int dibSize = ReadInt32LE(bmpBytes, 14);
+            int width = ReadInt32LE(bmpBytes, 18);
+            int heightRaw = ReadInt32LE(bmpBytes, 22);
+            int planes = ReadUInt16LE(bmpBytes, 26);
+            int bpp = ReadUInt16LE(bmpBytes, 28);
+            int compression = ReadInt32LE(bmpBytes, 30);
+
+            if (dibSize != BitmapInfoHeaderSize || planes != 1 || bpp != 32 || compression != 0)
+                return false;
+            if (pixelOffset < BitmapFileHeaderSize + BitmapInfoHeaderSize || width <= 0 || heightRaw == 0)
+                return false;
+
+            int height = heightRaw > 0 ? heightRaw : -heightRaw;
+            bool bottomUp = heightRaw > 0;
+            int srcStride = checked(width * 4);
+            long pixelBytes = (long)srcStride * height;
+            if (pixelOffset < 0 || pixelOffset + pixelBytes > bmpBytes.Length)
+                return false;
+
+            bmp = ImageUtils.CreateArgbBitmap(width, height, out var bmpData, out int stride);
+            Bitmap createdBmp = bmp;
+            try
+            {
+                byte[] row = new byte[width * 4];
+
+                for (int y = 0; y < height; y++)
+                {
+                    int srcY = bottomUp ? (height - 1 - y) : y;
+                    int srcRow = pixelOffset + srcY * srcStride;
+
+                    for (int x = 0; x < width; x++)
+                    {
+                        int src = srcRow + x * 4;
+                        int dst = x * 4;
+
+                        byte a = bmpBytes[src + 0];
+                        byte b = bmpBytes[src + 1];
+                        byte g = bmpBytes[src + 2];
+                        byte r = bmpBytes[src + 3];
+
+                        row[dst + 0] = b;
+                        row[dst + 1] = g;
+                        row[dst + 2] = r;
+                        row[dst + 3] = a;
                     }
 
                     ImageUtils.CopyRowToBitmap(bmpData, y, row, stride);
